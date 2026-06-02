1a4b99e4c6f5e8dc78c6210233989929fa9a3a20
Rohit Ware <rohitware99@gmail.com>
2026-06-03 00:21:09 +0530

    learn: add components and props practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import JSXBasics from "./jsx-basic/JSXBasics";
 import JSXBasics2 from "./jsx-basic/JSXBasics2";
+import Greeting from "./component-props/Greeting";
+import Card from "./component-props/Card";
+import Parent from "./component-props/Parent";
+
 function App() {
   return (
     <div>
       {/* <JSXBasics /> */}
-      <JSXBasics2 />
+      {/* <JSXBasics2 /> */}
+      {/* <Greeting /> */}
+      <Card title="React" text="A JS library for building UIs" />
+      <Card title="JavaScript" text="The language of the web" />
+      <Card title="HTML" text="Structure of a webpage" />
+
+      <Parent />
     </div>
   );
 }
---
+++ src/component-props/Parent.jsx
@@ -0,0 +1,38 @@
+function Son(props) {
+  return (
+    <div style={{ background: "lightgreen" }}>
+      <h2>Son</h2>
+      <div>{props.children}</div>
+    </div>
+  );
+}
+
+function Daughter(props) {
+  return (
+    <div style={{ background: "lightblue" }}>
+      <h2>Daughter</h2>
+      <div>{props.children}</div>
+    </div>
+  );
+}
+
+function Parent() {
+  return (
+    <div>
+      <h1>My two Children</h1>
+      <Son>
+        <p>
+          This was written in the Parent component, but displayed as a part of
+          the Son component
+        </p>
+      </Son>
+      <Daughter>
+        <p>
+          This was written in the Parent component, but displayed as a part of
+          the Daughter component
+        </p>
+      </Daughter>
+    </div>
+  );
+}
+export default Parent;
